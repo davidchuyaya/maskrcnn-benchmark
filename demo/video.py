ed4a7e24c3455ac74de99d657c76b6bbeca72b84
David Chu <dc788@hinton.cs.cornell.edu>
2018-11-04 16:50:01 -0500

    remove unnecessary outputs, GPU works

--- demo/video.py
+++ demo/video.py
@@ -8,7 +8,7 @@ config_file = "../configs/caffe2/e2e_mask_rcnn_R_50_FPN_1x_caffe2.yaml"
 # update the config options with the config file
 cfg.merge_from_file(config_file)
 # manual override some options
-cfg.merge_from_list(["MODEL.DEVICE", "cpu"])
+cfg.merge_from_list(["MODEL.DEVICE", "cuda"])
 
 coco_demo = COCODemo(
     cfg,
@@ -21,16 +21,16 @@ coco_demo = COCODemo(
 cam = cv.VideoCapture("video_0001.mp4")
 codec = cv.VideoWriter_fourcc(*"MJPG")
 out = cv.VideoWriter("output.avi", codec, 30.0, (int(cam.get(3)), int(cam.get(4))))
-#i = 0
+i = 0
 while True:
     ret, img = cam.read()
     if not ret:
         break
     predictions = coco_demo.run_on_opencv_image(img)
 #    cv.imwrite("./video/" + str(i) + ".png", predictions)
-#    i += 1
-#    if i == 10:
-#        break
+    i += 1
+    if i == 10:
+        break
     out.write(predictions)
 cam.release()
 out.release()
