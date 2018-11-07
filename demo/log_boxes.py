8e2fc1a6f89a6f75ffd1685babd0ea9d91b4e439
David Chu <davidchuyaya@gmail.com>
2018-11-06 23:56:30 -0500

    log_boxes now takes a directory of images & writes bounding box files for each one (one by one to not run out of memory)

--- demo/log_boxes.py
+++ demo/log_boxes.py
@@ -1,35 +1,38 @@
 from maskrcnn_benchmark.config import cfg
 from predictor import COCODemo
-import requests
-from io import BytesIO
 from PIL import Image
 import numpy as np
 import sys
+import glob
 import json
 
+# call with <directory> <mode>
+# directory. Must only contain image files.
+directory: str = sys.argv[1]
 # mode = "cpu" or "cuda"
-mode = sys.argv[1]
+mode: str = sys.argv[2]
 
-config_file = "../configs/caffe2/e2e_mask_rcnn_R_50_FPN_1x_caffe2.yaml"
 
 # update the config options with the config file
-cfg.merge_from_file(config_file)
+cfg.merge_from_file("../configs/caffe2/e2e_mask_rcnn_R_50_FPN_1x_caffe2.yaml")
 # manual override some options
 cfg.merge_from_list(["MODEL.DEVICE", mode])
-
 coco_demo = COCODemo(
     cfg,
     min_image_size=800,
     confidence_threshold=0.7,
 )
 
-def load(url):
-    """
-    Given an url of an image, downloads the image and
-    returns a PIL image
-    """
-    response = requests.get(url)
-    pil_image = Image.open(BytesIO(response.content)).convert("RGB")
+
+def get_file_names() -> list:
+    types = ["*.jpg", "*.png"]
+    names = []
+    for extension in types:
+        names += glob.glob1(directory, extension)
+    return names
+
+def load(filename: str):
+    pil_image = Image.open(filename).convert("RGB")
     # convert to BGR format
     image = np.array(pil_image)[:, :, [2, 1, 0]]
     return image
@@ -38,7 +41,7 @@ def predict(img):
     predictions = coco_demo.compute_prediction(img)
     return coco_demo.select_top_predictions(predictions)
 
-def save(predictions):
+def save(predictions, filename: str):
     labels = predictions.get_field("labels").tolist()
     bounding_boxes = predictions.bbox.tolist()
     json_objs = []
@@ -48,11 +51,13 @@ def save(predictions):
         bounding_box = bounding_boxes[i]
         json_objs.append({"label": label, "bbox": bounding_box})
 
-    f = open("out.json", "w")
+    json_name = filename.split(".")[0]
+    f = open(directory + "/" + json_name + ".json", "w")
     json.dump(json_objs, f)
     f.close()
 
 # load image and then run prediction
-image = load("https://user-images.githubusercontent.com/3080674/29361099-52eb370c-8286-11e7-8274-ceb4895fe0b9.png")
-predictions = predict(image)
-save(predictions)
+for f in get_file_names():
+    image = load(f)
+    predictions = predict(image)
+    save(predictions, f)
