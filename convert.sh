7e0a20e7f2cec73c50f2ea6574b45fcedb51cf0f
David Chu <dc788@hinton.cs.cornell.edu>
2018-11-27 16:28:34 -0500

    Removed unnecessary image dirs

--- convert.sh
+++ convert.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-rm -rf out
-mkdir out
 python convert_cityscapes_to_coco.py /scratch/datasets/cityscapes /scratch/datasets/cityscapes
 
 #rename directories
--- train.sh
+++ train.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# python tools/train_net.py --config-file "configs/e2e_mask_rcnn_R_101_FPN_1x.yaml" SOLVER.IMS_PER_BATCH 2 SOLVER.BASE_LR 0.0025 SOLVER.MAX_ITER 720000 SOLVER.STEPS "(480000, 640000)" TEST.IMS_PER_BATCH 1
-python -m torch.distributed.launch --nproc_per_node=2 tools/train_net.py --config-file "configs/e2e_mask_rcnn_R_101_FPN_1x.yaml" SOLVER.IMS_PER_BATCH 4 SOLVER.BASE_LR 0.005 SOLVER.MAX_ITER 360000 SOLVER.STEPS "(240000, 320000)" TEST.IMS_PER_BATCH 2
+python -m torch.distributed.launch --nproc_per_node=2 tools/train_net.py --config-file "configs/caffe2/e2e_mask_rcnn_R_101_FPN_1x_caffe2.yaml" SOLVER.IMS_PER_BATCH 4 SOLVER.BASE_LR 0.005 SOLVER.MAX_ITER 360000 SOLVER.STEPS "(240000, 320000)" TEST.IMS_PER_BATCH 2
 echo "done boiz"
