018b22b29bee72ebf0be1a61bc933b0f812a3b07
David Chu <davidchuyaya@gmail.com>
2018-12-04 20:39:53 -0500

    Find local cues from segmentation

--- demo/find_local_cues.py
+++ demo/find_local_cues.py
@@ -4,15 +4,17 @@ import sys
 import json
 import os
 
-# python find_local_cues.py <dataset directory> <mask directory>
+# python find_local_cues.py <dataset dir> <instance mask dir> <semantic mask dir> <output dir>
 """
 dataset directory must include:
 - pedestrian_dataset_folds/ with directories fold1/ to fold5/, and fold_dict.json
-mask directory must include:
+mask directories must include:
 - video_0001/, video_0002/, etc for each video in which a pedestrian is present in JAAD
 """
 dataDir: str = sys.argv[1]
-maskDir: str = sys.argv[2]
+instanceMaskDir: str = sys.argv[2]
+semanticMaskDir: str = sys.argv[3]
+outDir: str = sys.argv[4]
 
 # Resize pedestrian bounding box to height * 2 (this is the region in which we look for cues)
 def expandBbox(topLeft, bottomRight):
@@ -28,21 +30,41 @@ def expandBbox(topLeft, bottomRight):
 	newBottomRightY = min(centerY + height * 2, 1080)
 	return (newTopLeftX, newTopLeftY, newBottomRightX, newBottomRightY)
 
-def featuresInBbox(bbox, mask):
+def featuresInBbox(bbox, mask, mask_type, percentage=0.05):
 	(topLeftX, topLeftY, bottomRightX, bottomRightY) = bbox
-	patch = mask[topLeftX:bottomRightX, topLeftY:bottomRightY]
-	labels, counts = np.unique(patch, return_counts=True)
 	
-	threshold = patch.size * 0.05
-	labels = labels[counts >= threshold]
-	
-	return {
-		"road": False,
-		"sidewalk": False,
-		"traffic sign": 50 in labels,
-		"vehicle": 100 in labels,
-		"traffic light": 150 in labels
-	}
+	if mask_type == 'semantic':
+		if mask is None:
+			return {
+				"road": False,
+				"sidewalk": False
+			}
+			
+		topLeftX, topLeftY, bottomRightX, bottomRightY = bbox
+		patch = mask[topLeftY : bottomRightY, topLeftX : bottomRightX]
+		labels, counts = np.unique(patch, return_counts=True)
+		
+		threshold = int(patch.size * percentage)
+		labels = labels[counts >= threshold]
+		
+		return {
+			"road": 0 in labels,
+			"sidewalk": 1 in labels,
+		}	
+	elif mask_type == 'instance':
+		patch = mask[topLeftX:bottomRightX, topLeftY:bottomRightY]
+		labels, counts = np.unique(patch, return_counts=True)
+		
+		threshold = int(patch.size * percentage)
+		labels = labels[counts >= threshold]
+		
+		return {
+			"traffic sign": 50 in labels,
+			"vehicle": 100 in labels,
+			"traffic light": 150 in labels
+		}
+	else:
+		return {}
 
 fold_dict_filename = '/pedestrian_dataset_folds/fold_dict.json'
 with open(dataDir + fold_dict_filename, 'r') as f:
@@ -53,20 +75,28 @@ for json_filename in fold_dict:
 	with open(json_path, 'r') as f:
 		ped_json = json.load(f)
 
-	videoDir = maskDir + "/" + ped_json['video']
+	instanceVideoDir = instanceMaskDir + "/" + ped_json['video']
+	semanticVideoDir = semanticMaskDir + "/" + ped_json['video']
 	frames = ped_json["frame_data"]
 	for i, frame in enumerate(frames):
 		index = frame["frame_index"]
 		bbox = expandBbox(frame["bb_top_left"], frame["bb_bottom_right"])
 		try:
-			mask = np.array(Image.open(videoDir + "/" + str(index) + ".png"))
-			features = featuresInBbox(bbox, mask)
+			instanceMask = np.array(Image.open(instanceVideoDir + "/" + str(index) + ".png"))
+			instanceFeatures = featuresInBbox(bbox, instanceMask, 'instance')
+		except:
+			print("Could not find " + instanceVideoDir + "/" + str(index) + ".png")
+			instanceFeatures = {"traffic sign": False, "vehicle": False, "traffic light": False}
+		try:
+			semanticMask = np.array(Image.open(semanticVideoDir + "/" + str(index) + ".png"))
+			semanticFeatures = featuresInBbox(bbox, semanticMask, 'semantic')
 		except IOError:
-			print("Could not find " + videoDir + "/" + str(index) + ".png")
-			features = {"road": False, "sidewalk": False, "traffic sign": False, "vehicle": False, "traffic light": False}
+			print("Could not find " + semanticVideoDir + "/" + str(index) + ".png")
+			semanticFeatures = {"road": False, "sidewalk": False}
 		# add features to JSON
-		ped_json["frame_data"][i].update(features)
+		ped_json["frame_data"][i].update(instanceFeatures)
+		ped_json["frame_data"][i].update(semanticFeatures)
 	
-	os.makedirs(maskDir + "/" + fold, exist_ok=True)
-	with open(maskDir + "/" + fold + "/" + json_filename, "w") as f:
+	os.makedirs(outDir + "/" + fold, exist_ok=True)
+	with open(outDir + "/" + fold + "/" + json_filename, "w") as f:
 		json.dump(ped_json, f)
